09980ec7390f0b08e3bb9397dd51cd364a004756
madomado <madonuko@outlook.com>
2024-02-09 02:02:05 +0800

    feat: initial commit

---
+++ terminol/colors.py
@@ -0,0 +1,53 @@
+reset = "\x1b[0m"
+bold = "\x1b[1m"
+italic = "\x1b[3m"
+url = "\x1b[4m"
+underline = "\x1b[4m"
+blink = "\x1b[5m"
+blink2 = "\x1b[6m"
+selected = "\x1b[7m"
+
+
+class Fg:
+    black = "\x1b[30m"
+    red = "\x1b[31m"
+    green = "\x1b[32m"
+    yellow = "\x1b[33m"
+    blue = "\x1b[34m"
+    violet = "\x1b[35m"
+    magenta = "\x1b[35m"
+    cyan = "\x1b[36m"
+    white = "\x1b[37m"
+
+    grey = "\x1b[90m"
+    red2 = "\x1b[91m"
+    green2 = "\x1b[92m"
+    yellow2 = "\x1b[93m"
+    blue2 = "\x1b[94m"
+    violet2 = "\x1b[95m"
+    beige2 = "\x1b[96m"
+    white2 = "\x1b[97m"
+
+    orange = "\x1b[38;2;255;135;0m"
+
+
+class Bg:
+    black = "\x1b[40m"
+    red = "\x1b[41m"
+    green = "\x1b[42m"
+    yellow = "\x1b[43m"
+    blue = "\x1b[44m"
+    violet = "\x1b[45m"
+    beige = "\x1b[46m"
+    white = "\x1b[47m"
+
+    grey = "\x1b[100m"
+    red2 = "\x1b[101m"
+    green2 = "\x1b[102m"
+    yellow2 = "\x1b[103m"
+    blue2 = "\x1b[104m"
+    violet2 = "\x1b[105m"
+    beige2 = "\x1b[106m"
+    white2 = "\x1b[107m"
+
+    orange = "\x1b[48;2;255;135;0m"
